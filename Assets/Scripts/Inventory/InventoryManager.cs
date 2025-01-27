66d754bd18f280ca416d0117316e564e687cb40f
Quinn-Hub <56490493+Quinn-Hub@users.noreply.github.com>
2025-01-27 10:28:08 -0800

    Update: Initial Inventory Start
    ==========
     Changelog
    ==========
    - Created new scene, "TestScene-Items" to test the inventory system.
    - Created a new scripts called "Inventory", "InventoryManager", and "CollectibleItem." Inventory contains the inventory functions, InventoryManager contains functions to use and manage the inventory. CollectibleItem script is used for storing variables to those collectibles.
    - Added tag called Collectible for the use of labeling objects as something to collect.

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    private Inventory inventory;
+
+    private void Start()
+    {
+        inventory = GetComponent<Inventory>();
+
+        // Add sample items to the inventory
+        inventory.AddItem("Fishing Rod", Inventory.ItemType.Tool, 1);
+        inventory.AddItem("Bait", Inventory.ItemType.Ingredient, 8);
+
+        // Print initial inventory contents
+        inventory.PrintInventory();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            CollectItem(collision.gameObject, Inventory.ItemType.Ingredient);
+        }
+    }
+
+    private void CollectItem(GameObject collectible, Inventory.ItemType type)
+    {
+        CollectibleItem collectibleItem = collectible.GetComponent<CollectibleItem>();
+
+        if (collectibleItem != null)
+        {
+            inventory.AddItem(collectibleItem.itemName, type, collectibleItem.quantity);
+            Destroy(collectible);
+
+            // Print new acquired item message
+            inventory.PrintItem(collectibleItem.itemName);
+
+            // Print updated inventory contents after collection
+            //inventory.PrintInventory();
+        }
+    }
+}
